9bf87ac09ee04eca6c1ffd793db98a4e7fc8fda4
Chunlea <ichunlea@me.com>
2021-04-18 15:15:55 -0700

    Init heroicons-ruby project

---
+++ lib/heroicons.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "heroicons/version"
+
+module Heroicons
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/heroicons/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Heroicons
+  VERSION = "0.1.0"
+end
