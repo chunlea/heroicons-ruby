cdb344900bccf6c075fa4bc619a45059b375a183
Chunlea <ichunlea@me.com>
2021-04-18 17:09:02 -0700

    Load all heroicons on initializations

--- lib/heroicons.rb
+++ lib/heroicons.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "heroicons/version"
+require_relative "heroicons/icon"
+require "json"
 
 module Heroicons
-  class Error < StandardError; end
-  # Your code goes here...
+  file_data = File.read(File.join(File.dirname(__FILE__), "./heroicons/data.json"))
+  ICON_SYMBOLS = JSON.parse(file_data).freeze
 end
--- test/heroicons_test.rb
+++ test/heroicons_test.rb
@@ -7,7 +7,20 @@ class HeroiconsTest < Minitest::Test
     refute_nil ::Heroicons::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_loads_all_heroicons_on_initializations
+    refute_equal 0, Heroicons::ICON_SYMBOLS.length
+
+    x_icon = Heroicons::ICON_SYMBOLS["x"]
+    assert x_icon
+    assert x_icon["name"]
+    assert x_icon["variants"]
+    assert x_icon["variants"]["outline"]
+    assert x_icon["variants"]["outline"]["width"]
+    assert x_icon["variants"]["outline"]["height"]
+    assert x_icon["variants"]["outline"]["path"]
+    assert x_icon["variants"]["solid"]
+    assert x_icon["variants"]["solid"]["width"]
+    assert x_icon["variants"]["solid"]["height"]
+    assert x_icon["variants"]["solid"]["path"]
   end
 end
